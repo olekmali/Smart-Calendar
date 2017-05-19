bfb9ad3b9793466a391769cca1dd0e877d22133c
olekmali <olekmali@ieee.org>
2017-05-18 22:38:23 -0500

    random showing other calendar pages now shows the calendar more frequently

--- HTML/slideshow.js
+++ HTML/slideshow.js
@@ -8,21 +8,26 @@ function DisplayNext()
     if (slideIndex >= x.length)
     {
         slideIndex = 0;
-        var r = Math.floor(Math.random() * 5);
+        var r = Math.floor(Math.random() * 8);
         switch (r)
         {
             case 0:
             case 1:
+            case 2:
                 window.location.replace("index.html");
                 break;
-            case 2:
+            case 3:
+            case 4:
                 window.location.replace("newsfeed.html");
                 break;
-            case 3:
-                window.location.replace("power.html");
-                break;
-            case 4:
+            case 5:
                 window.location.replace("weather.html");
+                break;
+            case 6:
+                window.location.replace("radar.html");
+                break;
+            case 7:
+                window.location.replace("forecast.html");
                 break;
             default:
                 window.location.replace("index.html");
