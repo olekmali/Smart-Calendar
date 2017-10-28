7bd0a49efe285c946f37799b67ad757948d06878
olekmali <olekmali@ieee.org>
2017-10-28 01:50:54 -0500

    Added Astronomical Clock - for now it is just a sunclock

--- HTML/slideshow.js
+++ HTML/slideshow.js
@@ -8,26 +8,30 @@ function DisplayNext()
     if (slideIndex >= x.length)
     {
         slideIndex = 0;
-        var r = Math.floor(Math.random() * 8);
+        var r = Math.floor(Math.random() * 10);
         switch (r)
         {
             case 0:
             case 1:
             case 2:
+            case 3:
                 window.location.replace("index.html");
                 break;
-            case 3:
             case 4:
+            case 5:
                 window.location.replace("newsfeed.html");
                 break;
-            case 5:
+            case 6:
                 window.location.replace("weather.html");
                 break;
-            case 6:
+            case 7:
                 window.location.replace("radar.html");
                 break;
-            case 7:
+            case 8:
                 window.location.replace("forecast.html");
+                break;
+            case 9:
+                window.location.replace("clock.html");
                 break;
             default:
                 window.location.replace("index.html");
